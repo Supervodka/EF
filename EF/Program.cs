56646bb5b546f5734d04eae28b9118123b756a20
Кулик Роман Викторович <Kulik_R@ipps.by>
2022-05-04 18:15:08 +0300

    heze

--- EF/Program.cs
+++ EF/Program.cs
@@ -1,11 +1,11 @@
-﻿using EF;
-using System;
+﻿using System;
 using System.Linq;
-
+using EF;
 using (ApplicationContext db = new ApplicationContext())
-{
-    User user1 = db.Users.FirstOrDefault();
-    User user2 = db.Users.LastOrDefault();
-    db.Users.RemoveRange(user1,user2);
+            {
+                User user1 = db.Users.OrderByDescending().FirstOrDefault();
+                db.Users.Remove(user1);
+                db.SaveChanges();
+                Console.WriteLine("Success");
 }
-Console.ReadKey();
+    Console.Read();

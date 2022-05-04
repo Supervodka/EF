0302f7d8f4740a135d45f444b67ced25fd39d3b0
Кулик Роман Викторович <Kulik_R@ipps.by>
2022-05-04 17:44:36 +0300

    Add project files.

---
+++ EF/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Runtime.InteropServices;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF
+{
+    public class ApplicationContext :DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=WS-779;Database=EFdb;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ EF/Program.cs
@@ -0,0 +1,11 @@
+﻿using EF;
+using System;
+using System.Linq;
+
+using (ApplicationContext db = new ApplicationContext())
+{
+    User user1 = db.Users.FirstOrDefault();
+    User user2 = db.Users.LastOrDefault();
+    db.Users.RemoveRange(user1,user2);
+}
+Console.ReadKey();

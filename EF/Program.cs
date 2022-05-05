bf2ce78668fe5cf6d93a3cba5aa4082b74ca0d21
Кулик Роман Викторович <Kulik_R@ipps.by>
2022-05-05 10:13:06 +0300

    question

--- EF/Program.cs
+++ EF/Program.cs
@@ -1,11 +1,82 @@
 ﻿using System;
 using System.Linq;
 using EF;
-using (ApplicationContext db = new ApplicationContext())
+using System;
+using System.Linq;
+
+
+            // Добавление
+            using (ApplicationContext db = new ApplicationContext())
             {
-                User user1 = db.Users.OrderByDescending().FirstOrDefault();
-                db.Users.Remove(user1);
+                User user1 = new User { Name = "Tom", Age = 33 };
+                User user2 = new User { Name = "Alice", Age = 26 };
+
+                // Добавление
+                db.Users.Add(user1);
+                db.Users.Add(user2);
+                db.SaveChanges();
+            }
+
+            // получение
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                // получаем объекты из бд и выводим на консоль
+                var users = db.Users.ToList();
+                Console.WriteLine("Данные после добавления:");
+                foreach (User u in users)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                }
+            }
+
+            // Редактирование
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                // получаем первый объект
+                User user = db.Users.FirstOrDefault();
+                if (user != null)
+                {
+                    user.Name = "Bob";
+                    user.Age = 44;
+                    //обновляем объект
+                    //db.Users.Update(user);
+                    db.SaveChanges();
+                }
+                // выводим данные после обновления
+                Console.WriteLine("\nДанные после редактирования:");
+                var users = db.Users.ToList();
+                foreach (User u in users)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                }
+            }
+
+            // Удаление
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                // получаем первый объект
+                User user = db.Users.FirstOrDefault();
+                if (user != null)
+                {
+                    //удаляем объект
+                    db.Users.Remove(user);
+                    db.SaveChanges();
+                }
+                // выводим данные после обновления
+                Console.WriteLine("\nДанные после удаления:");
+                var users = db.Users.ToList();
+                foreach (User u in users)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                }
+            }
+            Console.Read();
+        // вот тут вопросик имееццо
+using (ApplicationContext db = new ApplicationContext()) 
+{
+                User user1 = db.Users.FirstOrDefault();
+                User user2 = db.Users.FirstOrDefault();
+    db.Users.RemoveRange(user1,user2);
                 db.SaveChanges();
                 Console.WriteLine("Success");
-}
-    Console.Read();
+} Console.Read();
